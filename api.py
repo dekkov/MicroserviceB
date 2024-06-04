9c6440bb5cef86af2857b109cc6e996330705622
Hoang Tran <trhoang2003@gmail.com>
2024-06-04 10:23:20 -0700

    Create api.py

---
+++ api.py
@@ -0,0 +1,35 @@
+from flask import Flask, request, jsonify
+from random import choice,randint,shuffle
+import os
+
+
+def generate_password():
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+    password_letters = [choice(letters) for _ in range(randint(8, 10))]
+    password_symbols = [choice(symbols) for _ in range(randint(2, 4))]
+    password_numbers = [choice(numbers) for _ in range(randint(2, 4))]
+
+    password_list = password_letters + password_symbols + password_numbers
+    shuffle(password_list)
+
+    password = "".join(password_list)
+    response = {
+            "Password": password,
+            "Status": "Done. Thank you!",
+        }
+    return response
+
+app = Flask(__name__)
+
+@app.route("/generate/")
+def get_user():
+    response = generate_password()
+    
+
+    return jsonify(response), 200
+
+if __name__ == "__main__":
+    app.run(host="127.0.0.1", port=8080,debug=True)
